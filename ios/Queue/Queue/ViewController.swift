5364cfae81c63be15fc5c1542a9492793103ad5c
Aleksander Grzyb <aleksander.grzyb@allegrogroup.com>
2016-09-24 18:26:00 +0200

    add state to VC

--- ios/Queue/Queue/ViewController.swift
+++ ios/Queue/Queue/ViewController.swift
@@ -6,9 +6,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    enum State {
+        case loading
+        case loaded([Deparment])
+        case empty
+        case error
+    }
+
+    var state: State = .loading
+
     override func viewDidLoad() {
         super.viewDidLoad()
         FirebaseClient().load(resource: Deparment.all) { result in
+            if let result = result {
+                self.state = .loaded(result)
+            } else {
+                self.state = .error
+            }
+            print(result)
+        }
 
         RemoteNotificationsManager.register { (result) in
             print(result)
