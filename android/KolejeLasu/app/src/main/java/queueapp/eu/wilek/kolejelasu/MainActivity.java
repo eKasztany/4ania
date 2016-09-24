20ceaf05283abd18228de6b331383a4d102a8ca2
Mateusz Wilczyński <wilek02@gmail.com>
2016-09-24 17:24:59 +0200

    Firebase config

--- android/KolejeLasu/app/src/main/java/queueapp/eu/wilek/kolejelasu/MainActivity.java
+++ android/KolejeLasu/app/src/main/java/queueapp/eu/wilek/kolejelasu/MainActivity.java
@@ -2,8 +2,18 @@ package queueapp.eu.wilek.kolejelasu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private DatabaseReference rootReference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
