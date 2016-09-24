f6ab9f7fa564530a1f5833931fb24efd2a8aa887
Aleksander Grzyb <aleksander.grzyb@allegrogroup.com>
2016-09-24 16:26:58 +0200

    add Firebase configuration

--- ios/Queue/Queue/AppDelegate.swift
+++ ios/Queue/Queue/AppDelegate.swift
@@ -4,6 +4,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FIRApp.configure()
         return true
     }
 }
