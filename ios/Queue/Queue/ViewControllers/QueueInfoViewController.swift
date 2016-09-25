8cf9b308cbaeb1e39281f9d6e62e42759733099d
Aleksander Grzyb <aleksander.grzyb@allegrogroup.com>
2016-09-25 08:46:58 +0200

    refactor queue screen

--- ios/Queue/Queue/ViewControllers/QueueInfoViewController.swift
+++ ios/Queue/Queue/ViewControllers/QueueInfoViewController.swift
@@ -10,15 +10,8 @@ import UserNotificationsUI
 class QueueInfoViewController: UIViewController {
 
     @IBOutlet weak var numberOfPeopleInQueue: UILabel!
-    @IBOutlet weak var numberOfOpenPositions: UILabel!
     @IBOutlet weak var currentNumber: UILabel!
-    @IBOutlet weak var averageTimeForService: UILabel!
     @IBOutlet weak var estimatedTimeOfService: UILabel!
-
-    @IBOutlet weak var minutesTextField: UITextField!
-    @IBOutlet weak var peopleTextField: UITextField!
-    @IBOutlet weak var minutesLabel: UILabel!
-    @IBOutlet weak var peopleLabel: UILabel!
 
     var service: ServiceGroup?
     override func viewDidLoad() {
@@ -30,14 +23,7 @@ class QueueInfoViewController: UIViewController {
     private func updateService() {
         guard let service = service else { return }
         numberOfPeopleInQueue.text = String(format: "%.0f", service.numberOfPeopleInQueue)
-        numberOfOpenPositions.text = String(format: "%.0f", service.numberOfAvailablePosition)
-        currentNumber.text = String(format: "%.0f", service.currentServiceNumber)
-        if let _ = service.serviceTime.range(of: ":") {
-            let serviceTimeWithMin = service.serviceTime.replacingOccurrences(of: ":", with: " min ")
-            averageTimeForService.text = serviceTimeWithMin.appending(" s")
-        } else {
-            averageTimeForService.text = "00 min 0 s"
-        }
+        currentNumber.text = "aktualny numer = " + String(format: "%.0f", service.currentServiceNumber)
         estimatedTimeOfService.text = calculateEstimatedTime()
     }
 
@@ -52,8 +38,11 @@ class QueueInfoViewController: UIViewController {
         let estimatedTime = totalSeconds.multiplying(by: NSDecimalNumber(value: peoplePerPosition))
         let estimatedTimeDouble = estimatedTime.doubleValue
         let estimatedMinutes = Int(estimatedTimeDouble / 60)
-        let estimatedSeconds = estimatedTimeDouble - Double(estimatedMinutes * 60)
-        return String(format: "%d", estimatedMinutes) + " min " + String(format: "%.0f", estimatedSeconds) + " s"
+        let _ = estimatedTimeDouble - Double(estimatedMinutes * 60)
+        return String(format: "%d", estimatedMinutes) + " min "
+    }
+
+    func showData() {
     }
 
     private func configureBarButtonItem() {
@@ -64,37 +53,6 @@ class QueueInfoViewController: UIViewController {
         let attributes = [NSFontAttributeName: UIFont.init(name: "FontAwesome", size: 20.0)!]
         barButtonItem.setTitleTextAttributes(attributes, for: .normal)
         navigationItem.rightBarButtonItem = barButtonItem
-    }
-
-    @IBAction func peopleNotification(_ sender: UIButton) {
-        sender.setTitle("Ustawiono!", for: .normal)
-        sender.setTitleColor(.green, for: .normal)
-        sender.isEnabled = false
-        peopleLabel.alpha = 0.0
-        peopleTextField.alpha = 0.0
-        peopleTextField.isEnabled = false
-        scheduleLocalNotification(message: "Zostało już 8 osób w kolejce.")
-    }
-
-    @IBAction func timeNotification(_ sender: UIButton) {
-        sender.setTitle("Ustawiono!", for: .normal)
-        sender.setTitleColor(.green, for: .normal)
-        sender.isEnabled = false
-        minutesLabel.alpha = 0.0
-        minutesTextField.alpha = 0.0
-        minutesTextField.isEnabled = false
-        scheduleLocalNotification(message: "Zostało już 5 minut do estymowanego czasu obsłużenia.")
-    }
-
-    @IBAction func myTurnNotificaton(_ sender: UIButton) {
-        sender.setTitle("Ustawiono!", for: .normal)
-        sender.setTitleColor(.green, for: .normal)
-        sender.isEnabled = false
-        scheduleLocalNotification(message: "Nadeszła Twoja kolej! Podejdź do okienka.")
-    }
-
-    func showData() {
-
     }
 
     private func scheduleLocalNotification(message: String) {
